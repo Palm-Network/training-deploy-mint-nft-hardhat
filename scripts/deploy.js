468bc336864236c6cb0876b12565374e214b64dd
anegg0 <gael.blanchemain@protonmail.com>
2022-01-18 20:02:34 -0500

    Nft > nft

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ async function main() {
     const NFT = await ethers.getContractFactory("NFT");
     
     // Start deployment, returning a promise that resolves to a contract object
-    const Nft = await NFT.deploy();
+    const nft = await NFT.deploy();
     console.log("Contract deployed to address:", Nft.address);
     }
     
